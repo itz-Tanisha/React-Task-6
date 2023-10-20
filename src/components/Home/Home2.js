de718bf652d1c2282c407dff5ee10f2179d4b52d
Tanisha <tanishamehta3020@gmail.com>
2023-10-20 14:53:26 +0530

    update

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.png";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" , fontWeight: "900" }}>
+              LET ME  INTRODUCE MYSELF
+            </h1>
+            <p className="home-about-body">
+      
+            <ul>
+                <li>3 rd Year Btech Student in CSE with Specialization in AI ML</li><br />
+                <li>Aspiring Software Developer</li><br />
+                <li>MERN Stack Developer</li><br />
+                <li>Hobbies include singing, finding joy in music, and enjoying badminton for physical activity and camaraderie.</li><br />
+                <li>Let's chat and see if we can team up on some awesome projects!</li>
+              </ul>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/itz-Tanisha"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              
+              <li className="social-icons">
+                <a
+                  href="https://in.linkedin.com/in/tanisha-mehta-428071219"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+             
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
